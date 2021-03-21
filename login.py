b634d794305ade872d237ebb410afbfd7c9d5621
Hito <413168358@qq.com>
2021-03-21 13:16:16 +0800

    修改login.py

--- login.py
+++ login.py
@@ -81,7 +81,11 @@ def usr_sign_up():
                 if usr_pwd.get()!=confirm_pwd.get():
                     tk.messagebox.showerror(title='Sign up',message='两次输入的密码不一致，请重新输入')
                 else:
-                    tk.messagebox.showinfo(message='congratulation')
+                    usr_file=open('usrs_info.pickle','wb')
+                    usrs_info[usr_name.get()]=usr_pwd.get() #将用户注册的账号密码保存入usrs_info
+                    pickle.dump(usrs_info,usr_file) #把用户输入保存入pickle file
+                    usr_file.close()
+                    tk.messagebox.showinfo(message='congratulations, you have successfully sign up')
 
 
     tk.Button(window_sign_up,text='ok',width=7,command=sign_up_check).place(x=70,y=145)
